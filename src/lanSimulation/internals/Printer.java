87b8157e114fa6043d3e769c96dd0ab668ee9d5e
SergioLopez92 <slb0011@alu.ubu.es>
2018-06-01 13:06:20 +0200

    19: Creadas subclases vacías
    Crea subclases vacías para los diferentes tipos de nodo

---
+++ src/lanSimulation/internals/Printer.java
@@ -0,0 +1,14 @@
+package lanSimulation.internals;
+
+public class Printer extends Node {
+
+	public Printer(byte type, String name, Node nextNode) {
+		super(type, name, nextNode);
+		// TODO Apéndice de constructor generado automáticamente
+	}
+	
+	public Printer (byte type, String name){
+		super(type, name);
+	}
+
+}
---
+++ src/lanSimulation/internals/Workstation.java
@@ -0,0 +1,14 @@
+package lanSimulation.internals;
+
+public class Workstation extends Node {
+
+	public Workstation(byte type, String name) {
+		super(type, name);
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+	public Workstation(byte type, String name, Node nextNode){
+		super(type, name,nextNode);
+	}
+	
+}
